f4d8c402ac76a634d6aa9294b805072a45814378
Jose Miguel <jmmirand@gmail.com>
2022-05-02 17:46:48 +0200

    test debug method

--- .github/actions/ghalog_cleaner/src/main.js
+++ .github/actions/ghalog_cleaner/src/main.js
@@ -8,5 +8,20 @@ let i;
 for (i = 0; i < numOfRepeats; i++) {
     outputText += inputText;
 }
+
+
+try {
+  core.debug('Inside try block');
+  
+  core.warning('test Warning');
+
+  core.info('INFO Output to the actions build log')
+
+  core.notice('This is a message that will also emit an annotation')
+}
+catch (err) {
+  core.error(`Error ${err}, action may still succeed though`);
+}
+
  
 core.setOutput('output_text', outputText)
